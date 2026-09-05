976845c680c788a8edb397f1bab1f069e4431672
Sachin <sachin.sachin_cs25@gla.ac.in>
2026-09-05 10:23:34 +0530

    Time: 2 ms (15.33%) | Memory: 42.1 MB (38.37%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+    for(int i=1;i<=num/i;i++){
+        int r=i*i;
+        if(r==num)
+            return true;
+            }
+        
+           return  false;
+        
+    }
+ }
