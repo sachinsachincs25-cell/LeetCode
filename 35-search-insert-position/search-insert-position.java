f76680e3d7bb96d903466678fdb2bc041ee0910a
Sachin <sachin.sachin_cs25@gla.ac.in>
2026-09-17 00:00:22 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (48.47%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,12 +1,21 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] == target) {
-                return i;}
-                if(nums[i]>target){
-                    return i;}
-                    
-                    } 
-                return nums.length;
+        //int a=Arrays.binarySearch(nums,target);
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]<target){
+                low=mid+1;
+            }else
+            {
+                high=mid-1;
+            }
+        }
+        return low;
+        
     }
 }
