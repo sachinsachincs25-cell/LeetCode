49410bee97567a9a9eeca6710876868492d4237d
Sachin <sachin.sachin_cs25@gla.ac.in>
2026-09-24 20:16:03 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (28.51%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,13 +1,14 @@
 class Solution {
     public int search(int[] nums, int target) {
-
+        // Array ke har element ko check 
         for(int i=0;i<nums.length;i++){
-
+            // Agar current element target ke equal hai
             if(nums[i]==target){
+                // Target ka index return kar do
                 return i;
             }
         }
-
+        // Agar target array mein nahi mila
         return -1;
     }
 }
